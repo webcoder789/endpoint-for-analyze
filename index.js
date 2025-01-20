b846a9b8da68c7b93d82cbb8e9c146775dbf278c
webcoder789 <169659887+webcoder789@users.noreply.github.com>
2025-01-20 13:10:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { decycle } = require("json-cyclic");
+
+const app = express();
+
+app.use(express.json());
+
+// 通常のGETリクエスト
+app.post("/post", (req, res) => {
+  //   const reqJson = JSON.stringify(decycle(req), null, 2);
+  const reqJson = JSON.stringify(decycle(req));
+  console.log(reqJson);
+  console.log(
+    "----------------------------------------------------------------"
+  );
+
+  res.json({ message: "success" });
+});
+
+const port = 3000;
+// サーバーを起動
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
